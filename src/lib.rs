4fc7ad70cb869678089f05afa265e755cc96800a
Dan Gohman <dev@sunfishcode.online>
2021-11-09 12:43:36 -0800

    Remvoe use of `doc = include_str` for now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
-#![doc = include_str!("../README.md")]
+//! This crate provides a few miscellaneous utilities related to I/O:
+//!
+//! - `HandleOrSocket` types and traits for Windows, which abstract over Windows
+//!   `*Handle*` and their corresponding Windows `*Socket*` types and traits.
+//!
+//! - `Grip` types and traits, which abstract over the aforementioned Windows
+//!   `HandleOrSocket` types and traits and their corresponding non-Windows `Fd`
+//!   types and traits.
+//!
+//! - `RawReadable` and `RawWritable`, which adapt a raw `Fd`/`Handle` to
+//!   implement the `Read` and `Write` traits, respectively.
+//!
+//! - `ReadWrite` traits, and supporting types, which provide abstractions over
+//!   types with one or two I/O resources, for reading and for writing.
+
 #![deny(missing_docs)]
 #![cfg_attr(can_vector, feature(can_vector))]
 #![cfg_attr(write_all_vectored, feature(write_all_vectored))]
