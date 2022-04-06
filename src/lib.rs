0190af04287641401379543fa51d95b32b405413
Dan Gohman <dev@sunfishcode.online>
2022-04-06 07:49:31 -0700

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 //! This crate provides a few miscellaneous utilities related to I/O:
 //!
-//! - `HandleOrSocket` types and traits for Windows, which abstract over Windows
-//!   `*Handle*` and their corresponding Windows `*Socket*` types and traits.
+//! - `HandleOrSocket` types and traits for Windows, which abstract over
+//!   Windows `*Handle*` and their corresponding Windows `*Socket*` types and
+//!   traits.
 //!
 //! - `Grip` types and traits, which abstract over the aforementioned Windows
-//!   `HandleOrSocket` types and traits and their corresponding non-Windows `Fd`
-//!   types and traits.
+//!   `HandleOrSocket` types and traits and their corresponding non-Windows
+//!   `Fd` types and traits.
 //!
 //! - `RawReadable` and `RawWritable`, which adapt a raw `Fd`/`Handle` to
 //!   implement the `Read` and `Write` traits, respectively.
